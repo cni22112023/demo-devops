d77d4a9d6bfcfb8e6e9a844c0a3c995cc2750492
cni22112023 <151912403+cni22112023@users.noreply.github.com>
2024-12-03 13:35:43 +0000

    test

---
+++ dali/src/main/java/com/example/dali/DaliApplication.java
@@ -0,0 +1,13 @@
+package com.example.dali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaliApplication.class, args);
+	}
+
+}
